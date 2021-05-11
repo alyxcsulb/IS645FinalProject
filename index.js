79a01555f036347bf06f80f435d60c0f8834f584
Alyx Faber <alyx.faber@student.csulb.edu>
2021-05-10 20:11:53 -0700

    commit

--- index.js
+++ index.js
@@ -19,11 +19,7 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
-// Add database package and connection string (can remove ssl)
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+// testing out style sheets please ignore
+// <link href="/styles.css" rel="stylesheet" type="text/css">
+// <!-- CSS only -->
+// <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous"></link>
